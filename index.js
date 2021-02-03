abd8ee6d58a5510f8ab8dad961881a2ad3721433
Matt Autrey <mautrey17@gmail.com>
2021-02-02 19:27:11 -0500

    questions array

--- index.js
+++ index.js
@@ -7,36 +7,36 @@ const questions = [
         message: 'What is the project title',
         name: 'title'
     },
-    // {
-    //     type: 'input',
-    //     message: 'What is the project description?',
-    //     name: 'description'
-    // },
-    // {
-    //     type: 'input',
-    //     message: 'What are the installation instructions?',
-    //     name: 'installation'
-    // },
-    // {
-    //     type: 'input',
-    //     message: 'What information should be included about usage?',
-    //     name: 'usage'
-    // },
-    // {
-    //     type: 'input',
-    //     message: 'How should others users contribute to the project?',
-    //     name: 'contribution'
-    // },
-    // {
-    //     type: 'input',
-    //     message: 'How should the application be tested?',
-    //     name: 'test'
-    // },
-    // {
-    //     type: 'input',
-    //     message: 'What is your github username?',
-    //     name: 'github'
-    // },
+    {
+        type: 'input',
+        message: 'What is the project description?',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'What are the installation instructions?',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'What information should be included about usage?',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'How should others users contribute to the project?',
+        name: 'contribution'
+    },
+    {
+        type: 'input',
+        message: 'How should the application be tested?',
+        name: 'test'
+    },
+    {
+        type: 'input',
+        message: 'What is your github username?',
+        name: 'github'
+    },
     {
         type: 'input',
         message: 'What is your email?',
