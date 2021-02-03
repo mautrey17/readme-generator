c9aa3e8657b0049838aa89a64a8608946d6d8ca7
Matt Autrey <mautrey17@gmail.com>
2021-02-03 18:28:03 -0500

    moved readme contents to new function

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# readme-generator
-Command line application to create README.md for projects based on user needs and input
+# t
+
+[![License: ISC License](https://img.shields.io/badge/license-MIT%20License-red)](https://choosealicense.com/licenses/mit/)
+
+
+## Table of Contents
+1. [Description](#Description)
+2. [Installation](#Installation)
+3. [Usage](#Usage)
+4. [License](#License)
+5. [Contributing](#Contributing)
+6. [Questions](#Questions)
+
+## Description
+t
+
+## Installation
+In order to install the application, follow these steps: 
+
+t
+
+## Usage
+t
+
+## License
+This application is protected under MIT License
+
+## Contributing
+t
+
+## Tests
+In order to test the application, follow these steps:
+t
+
+## Questions
+You can find my other applications on my github page
+Github: [t](https://github.com/t) 
+Or you can email me at: t
--- index.js
+++ index.js
@@ -1,78 +1,79 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
 // const { listenerCount } = require('events');
 
 const questions = [
-    {
-        type: 'input',
-        message: 'What is the project title',
-        name: 'title'
-    },
-    {
-        type: 'input',
-        message: 'What is the project description?',
-        name: 'description'
-    },
-    {
-        type: 'input',
-        message: 'What are the installation instructions?',
-        name: 'installation'
-    },
-    {
-        type: 'input',
-        message: 'What information should be included about usage?',
-        name: 'usage'
-    },
-    {
-        type: 'input',
-        message: 'How should others users contribute to the project?',
-        name: 'contribution'
-    },
-    {
-        type: 'input',
-        message: 'How should the application be tested?',
-        name: 'test'
-    },
-    {
-        type: 'input',
-        message: 'What is your github username?',
-        name: 'github'
-    },
-    {
-        type: 'input',
-        message: 'What is your email?',
-        name: 'email'
-    },
-    {
-        type: 'list',
-        message: 'Which type of license would you like?',
-        name: 'license',
-        choices: ['Apache License 2.0', 'ISC License', 'MIT License', 'GNU General Public License v2.0', 'No license']
-    }
-]
+  {
+    type: "input",
+    message: "What is the project title",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "What is the project description?",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "What are the installation instructions?",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "What information should be included about usage?",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "How should others users contribute to the project?",
+    name: "contribution",
+  },
+  {
+    type: "input",
+    message: "How should the application be tested?",
+    name: "test",
+  },
+  {
+    type: "input",
+    message: "What is your github username?",
+    name: "github",
+  },
+  {
+    type: "input",
+    message: "What is your email?",
+    name: "email",
+  },
+  {
+    type: "list",
+    message: "Which type of license would you like?",
+    name: "license",
+    choices: [
+      "Apache License 2.0",
+      "ISC License",
+      "MIT License",
+      "GNU General Public License v2.0",
+      "No license",
+    ],
+  },
+];
 
-const getLicense = license =>{
-    switch(license){
-        case 'Apache License 2.0':
-            return '[![License: Apache License 2.0](https://img.shields.io/badge/license-Apache-brightgreen.svg)](https://choosealicense.com/licenses/apache-2.0/)';
-            break;
-        case 'ISC License':
-            return '[![License: ISC License](https://img.shields.io/badge/license-ISC%20License-blue)](https://choosealicense.com/licenses/isc/)';
-            break;
-        case 'MIT License': 
-            return '[![License: ISC License](https://img.shields.io/badge/license-MIT%20License-red)](https://choosealicense.com/licenses/mit/)';
-            break;
-        case 'MIT License': 
-            return '[![License: ISC License](https://img.shields.io/badge/license-GNU%20GPL%20v3-orange)](https://choosealicense.com/licenses/gpl-2.0/)';
-            break;
-        default:
-            console.log('no license selected')
-            return '';
-    }
-}
+const getLicense = (license) => {
+  switch (license) {
+    case "Apache License 2.0":
+      return "[![License: Apache License 2.0](https://img.shields.io/badge/license-Apache-brightgreen.svg)](https://choosealicense.com/licenses/apache-2.0/)";
+    case "ISC License":
+      return "[![License: ISC License](https://img.shields.io/badge/license-ISC%20License-blue)](https://choosealicense.com/licenses/isc/)";
+    case "MIT License":
+      return "[![License: ISC License](https://img.shields.io/badge/license-MIT%20License-red)](https://choosealicense.com/licenses/mit/)";
+    case "MIT License":
+      return "[![License: ISC License](https://img.shields.io/badge/license-GNU%20GPL%20v3-orange)](https://choosealicense.com/licenses/gpl-2.0/)";
+    default:
+      console.log("no license selected");
+      return "";
+  }
+};
 
-inquirer.prompt(questions).then(response =>{
-    fs.writeFileSync('sample.md', `# ${response.title}
+const generateReadme = (response) => `# ${response.title}
 
 ${getLicense(response.license)}
 
@@ -90,6 +91,7 @@ ${response.description}
 
 ## Installation
 In order to install the application, follow these steps: 
+
 ${response.installation}
 
 ## Usage
@@ -108,6 +110,13 @@ ${response.test}
 ## Questions
 You can find my other applications on my github page
 Github: [${response.github}](https://github.com/${response.github}) 
-Or you can email me at: ${response.email}`);
+Or you can email me at: ${response.email}`;
+
+const init = () => {
+  inquirer.prompt(questions).then((response) => {
+    fs.writeFileSync("README.md", generateReadme(response));
     console.log(response);
-})
+  });
+};
+
+init();
